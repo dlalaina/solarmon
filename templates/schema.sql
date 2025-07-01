9c72ba13bc2b922cdeffaf85135af1b9cdc810fa
Cronjob Schema bkp <cron@solarmon.local>
2025-07-01 13:01:01 -0300

    Auto-commit: Atualização do schema do banco de dados

--- templates/schema.sql
+++ templates/schema.sql
@@ -39,7 +39,7 @@ CREATE TABLE `alarms` (
   `cleared_by` varchar(50) DEFAULT NULL,
   `observation` text,
   PRIMARY KEY (`alarm_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -235,4 +235,4 @@ CREATE TABLE `solar_data` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-01 12:01:01
+-- Dump completed on 2025-07-01 13:01:01
