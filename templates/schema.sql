24b24be5a30b01f8975d131e1c97e04a9087e230
Cronjob Schema bkp <cron@solarmon.local>
2025-07-14 14:01:01 -0300

    Auto-commit: Atualização do schema do banco de dados

--- templates/schema.sql
+++ templates/schema.sql
@@ -47,7 +47,7 @@ CREATE TABLE `alarms` (
   `cleared_by` varchar(50) DEFAULT NULL,
   `observation` text,
   PRIMARY KEY (`alarm_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -255,7 +255,7 @@ CREATE TABLE `solar_data` (
   KEY `idx_plant_name` (`plant_name`),
   KEY `idx_inverter_last_update` (`inverter_id`,`last_update_time` DESC),
   KEY `idx_last_update_time` (`last_update_time`)
-) ENGINE=InnoDB AUTO_INCREMENT=5747 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5890 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
